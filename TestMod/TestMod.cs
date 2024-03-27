636e0f763d40092773b8dabdffae767b063cab1b
Casey Warrington <spacechase0.and.cat@gmail.com>
2024-03-27 18:57:10 -0500

    Test mod!

---
+++ TestMod/TestMod.cs
@@ -0,0 +1,12 @@
+﻿using StardewModdingAPI;
+
+namespace TestMod
+{
+    public class TestMod : StardewModdingAPI.Mod
+    {
+        public override void Entry(IModHelper helper)
+        {
+            Monitor.Log("Meow!", LogLevel.Info);
+        }
+    }
+}
